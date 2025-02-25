08b1700f12c219f690af2e70f81d575eb04a3115
Colin Peng <colin100113@gmail.com>
2025-02-24 19:12:59 -0500

    uuid

--- supabase/migrations/000014_update_student.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE Student RENAME TO Student_legacy;
-
-CREATE TABLE Student ( -- TODO: add UUID here and check.
-    id INTEGER PRIMARY KEY,
-    email VARCHAR(255) UNIQUE, -- Assuming emails are unique
-    country VARCHAR(255),
-    time_zone VARCHAR(255)
-);
