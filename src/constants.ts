24f34e160608cc1e855c85154330fe90da451dc4
kssampson <kylesampsonmusic@gmail.com>
2025-04-05 11:53:28 -0700

    adjusted constants to use 12pm-5pm, removing the space

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { StudentField } from "./api/types";
 
 const TIME_SLOTS: string[] = [
   "Friday morning (7am-12pm)",
-  "Friday afternoon (12pm-5 pm)",
+  "Friday afternoon (12pm-5pm)",
   "Friday evening (5pm-10pm)",
   "Saturday morning (7am-12pm)",
   "Saturday afternoon (12pm-5pm)",
