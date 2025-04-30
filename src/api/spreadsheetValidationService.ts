89e44c317ae402036c90f7e0a3d4d93e868e8bd5
kssampson <kylesampsonmusic@gmail.com>
2025-04-30 15:55:03 -0700

    refactored to remove deeper nesting logic

--- src/api/spreadsheetValidationService.ts
+++ src/api/spreadsheetValidationService.ts
@@ -1,72 +1,89 @@
-import { Student, TimeWindow, ValidationErrors } from "./types";
+import { Student, TimeWindow, ValidationError } from "./types";
 
 export class SpeadsheetValidationService {
 
-  validateName(name: string): boolean {
-    return name.trim().length >= 4;
+  validateName(name: string): null | ValidationError {
+    if (name.trim().length <= 3) {
+      return { isValid: false, field: 'name', message: 'Name must be at least four characters' }
+    }
+    return null;
   }
 
-  validateEmail(email: string): boolean {
-    if (!email) return false;
+  validateEmail(email: string): null | ValidationError {
     const regExp = new RegExp('^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9]+)+$');
-    return regExp.test(email);
+    if (!email || !regExp.test(email)) {
+      return { isValid: false, field: 'email', message: 'Invalid email format' }
+    }
+    return null;
   }
 
-  validateCity(city: string | undefined): boolean {
-    if (!city) return false;
-    return city.trim().length > 0;
+  validateCity(city: string | undefined): null | ValidationError {
+    if (city?.trim().length === 0) {
+      return { isValid: false, field: 'city', message: 'City is required' }
+    }
+    return null;
   }
 
-  validateCountry(country: string): boolean {
-    if (!country) return false;
-    return country.trim().length > 0;
+  validateCountry(country: string): null | ValidationError {
+    if (country.trim().length === 0) {
+      return { isValid: false, field: 'country', message: 'Country is required' }
+    }
+    return null;
   }
 
-  //may want specific errormessages made and return those
-  validateTimeWindows(timeWindows: Partial<TimeWindow>[]): boolean {
-    //uncomment when timeWindows is required
-   if (timeWindows && !timeWindows.length) return false;
+  validateTimeWindows(timeWindows: Partial<TimeWindow>[]): null | ValidationError[] {
+    const timeWindowErrors: ValidationError[] = [];
+    //FIXME: uncomment when timeWindows is required
+  //  if (timeWindows && !timeWindows.length) {
+  //   timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: 'Must include time window availabilities' });
+  //  }
 
    const validDays = ['Friday', 'Saturday', 'Sunday'];
-   //made from '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$' ensure failure for testing
    const validTimeFormatRegExp = new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$')
 
    for (const tw of timeWindows) {
     if (!tw.day_in_week || !validDays.includes(tw.day_in_week.trim())) {
-      return false;
+      timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Day of week must be 'Friday', 'Saturday', or 'Sunday'` });
     }
     if (!tw.start_t || !validTimeFormatRegExp.test(tw.start_t)) {
-      return false;
+      timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Start time invalid` });
     }
     if (!tw.end_t || !validTimeFormatRegExp.test(tw.end_t)) {
-      return false;
+      timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `End time invalid` });
     }
     //add more check here if desired: start date < end date, etc.
    }
 
-   return true;
+   return timeWindowErrors.length ? timeWindowErrors : null;
   }
 
-  validateStudent(student: Student): ValidationErrors {
-    //in using an array to hold multiple errors if needed later
-    const errors: { [key: string]: string[] } = {};
-    if (!this.validateName(student.name)) {
-      errors['name'] = [...errors['name'] || [], 'Name must be at least four characters'];
-    }
-    if (!this.validateEmail(student.email)) {
-      errors['email'] =  [...errors['email'] || [], 'Invalid email format'];
-    }
-    if (!this.validateCity(student.city)) {
-      errors['city'] = [...errors['city'] || [], 'City is required'];
-    }
-    if (!this.validateCountry(student.country)) {
-      errors['country'] = [...errors['country'] || [], 'Country is required'];
-    }
-    if (student.timeWindows && !this.validateTimeWindows(student.timeWindows!)) {
-      errors['timeWindows'] = ['TimeWindows contain invalid format']
-    }
-    return errors
+  validateStudent(student: Student): Partial<ValidationError>[] {
+    //validateStudent assembles error messages and return an array of all error, with a field and message
+    const validaitonErrors: Partial<ValidationError>[] = [];
 
+    const nameError = this.validateName(student.name);
+    if (nameError) {
+      validaitonErrors.push({ field: nameError.field, message: nameError.message });
+    }
+    const emailError = this.validateEmail(student.email);
+    if (emailError) {
+      validaitonErrors.push({ field: emailError.field, message: emailError.message });
+    }
+    const cityError = this.validateCity(student.city);
+    if (cityError) {
+      validaitonErrors.push({ field: cityError.field, message: cityError.message });
+    }
+    const countryError = this.validateCountry(student.country);
+    if (countryError) {
+      validaitonErrors.push({ field: countryError.field, message: countryError.message });
+    }
+    if (student.timeWindows) {
+      const timeWindowErrors = this.validateTimeWindows(student.timeWindows)
+      if (timeWindowErrors) {
+        validaitonErrors.push(...timeWindowErrors);
+      }
+    }
+    return validaitonErrors;
   }
 
 }
