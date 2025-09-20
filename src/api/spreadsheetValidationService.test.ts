2d053e83bbfab3c827ba61944bc6bfdf0deb202e
jnakaso <jnakaso@yahoo.com>
2025-09-19 21:25:41 -0700

    age validation and tests!

---
+++ src/api/spreadsheetValidationService.test.ts
@@ -0,0 +1,103 @@
+import { describe, expect, it } from "vitest";
+import { AgeValidator, CityValidator, CountryValidator, TimeWindowValidator } from "./spreadsheetValidationService";
+import { Student, TimeWindow } from "./types";
+
+describe("SpeadsheetValidationService", () => {
+
+    it("validate age", () => {
+        const validator = new AgeValidator();
+
+        const student = {
+            age: null
+        } as Student;
+        let response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+
+        student.age = 12;
+        response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+        expect(response[0].message).toBe('Age must between 13 and 19.');
+
+        student.age = 20;
+        response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+        expect(response[0].message).toBe('Age must between 13 and 19.');
+
+        student.age = 13;
+        response = validator.validate(student);
+        expect(response.length).toBe(0);
+
+    });
+
+    it("validate country", () => {
+        const validator = new CountryValidator();
+
+        const student = {
+            country: ""
+        } as Student;
+
+        let response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+        expect(response[0].message).toBe('Country is required');
+
+        student.country = "Peru";
+        response = validator.validate(student);
+        expect(response.length).toBe(0);
+
+    });
+
+    it("validate country", () => {
+        const validator = new CountryValidator();
+
+        const student = {
+            country: ""
+        } as Student;
+
+        let response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+        expect(response[0].message).toBe('Country is required');
+
+        student.country = "Peru";
+        response = validator.validate(student);
+        expect(response.length).toBe(0);
+
+    });
+
+    it("validate city", () => {
+        const validator = new CityValidator();
+
+        const student = {
+            city: ""
+        } as Student;
+
+        let response = validator.validate(student);
+        expect(response.length).toBe(1);
+        expect(response[0].isValid).toBe(false);
+        expect(response[0].message).toBe('City is required');
+
+        student.city = "Aiea";
+        response = validator.validate(student);
+        expect(response.length).toBe(0);
+
+    });
+
+    it("validate timewindows", () => {
+        const validator = new TimeWindowValidator();
+
+        const student = {
+            timeWindows: [] as TimeWindow[]
+        } as Student;
+
+        student.timeWindows = [{ day_in_week: "Friday", start_t: "07:00:00", end_t: "12:00:00" } as TimeWindow];
+        let response = validator.validate(student);
+        expect(response.length).toBe(0);
+
+    });
+
+
+});
--- src/api/spreadsheetValidationService.ts
+++ src/api/spreadsheetValidationService.ts
@@ -1,12 +1,12 @@
 import { Student, ValidationError } from "./types";
 
-interface Validator<T> {
-  valdiate(data: T): ValidationError[];
+export interface Validator<T> {
+  validate(data: T): ValidationError[];
 }
 
-class NameValidator implements Validator<Student> {
+export class NameValidator implements Validator<Student> {
   minLength = 3;
-  valdiate(student: Student): ValidationError[] {
+  validate(student: Student): ValidationError[] {
     if (student.name.trim().length <= this.minLength) {
       return [{ isValid: false, field: 'name', message: 'Name must be at least four characters' }]
     }
@@ -14,9 +14,9 @@ class NameValidator implements Validator<Student> {
   }
 }
 
-class EmailValidator implements Validator<Student> {
+export class EmailValidator implements Validator<Student> {
   regExp = new RegExp('^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9]+)+$');
-  valdiate(student: Student): ValidationError[] {
+  validate(student: Student): ValidationError[] {
     if (!student.email || !this.regExp.test(student.email)) {
       return [{ isValid: false, field: 'email', message: 'Invalid email format' }]
     }
@@ -24,43 +24,61 @@ class EmailValidator implements Validator<Student> {
   }
 }
 
-class CityValidator implements Validator<Student> {
-  minlength = 2;
-  valdiate(student: Student): ValidationError[] {
-    if (student.city!.trim().length <= this.minlength) {
+const MIN_CITY_LENGTH = 1;
+export class CityValidator implements Validator<Student> {
+  minlength = 1;
+  validate(student: Student): ValidationError[] {
+    if (student.city!.trim().length < MIN_CITY_LENGTH) {
       return [{ isValid: false, field: 'city', message: 'City is required' }]
     }
     return []
   }
 }
-class CountryValidator implements Validator<Student> {
-  minlength = 2;
-  valdiate(student: Student): ValidationError[] {
-    if (student.country.trim().length <= this.minlength) {
+
+const MIN_STUDENT_AGE = 13;
+const MAX_STUDENT_AGE = 19;
+
+export class AgeValidator implements Validator<Student> {
+  validate(student: Student): ValidationError[] {
+    if (!student.age) {
+      return [{ isValid: false, field: 'age', message: 'Student age is required.' }]
+    }
+    if (student.age && (student.age < MIN_STUDENT_AGE || student.age > MAX_STUDENT_AGE)) {
+      return [{ isValid: false, field: 'age', message: `Age must between ${MIN_STUDENT_AGE} and ${MAX_STUDENT_AGE}.` }]
+    }
+    return []
+  }
+}
+
+const MIN_COUNTRY_LENGTH = 1;
+export class CountryValidator implements Validator<Student> {
+  validate(student: Student): ValidationError[] {
+    if (student.country.trim().length < MIN_COUNTRY_LENGTH) {
       return [{ isValid: false, field: 'country', message: 'Country is required' }]
     }
     return []
   }
 }
 
-class TimeWindowValidator implements Validator<Student> {
-  timeWindowErrors: ValidationError[] = [];
+export class TimeWindowValidator implements Validator<Student> {
   validDays: string[] = ['Friday', 'Saturday', 'Sunday'];
   validTimeFormatRegExp: RegExp = new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$')
 
-  valdiate(student: Student): ValidationError[] {
+  validate(student: Student): ValidationError[] {
+    const timeWindowErrors: ValidationError[] = [];
+
     for (const tw of student.timeWindows!) {
       if (!tw.day_in_week || !this.validDays.includes(tw.day_in_week.trim())) {
-        this.timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Day of week must be "Friday", "Saturday", or "Sunday"}` });
+        timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Day of week must be "Friday", "Saturday", or "Sunday"` });
       }
       if (!tw.start_t || !this.validTimeFormatRegExp.test(tw.start_t)) {
-        this.timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Start time invalid` });
+        timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `Start time invalid` });
       }
       if (!tw.end_t || !this.validTimeFormatRegExp.test(tw.end_t)) {
-        this.timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `End time invalid` });
+        timeWindowErrors.push({ isValid: false, field: 'timeWindows', message: `End time invalid` });
       }
     }
-    return this.timeWindowErrors;
+    return timeWindowErrors;
   }
 }
 
@@ -68,6 +86,7 @@ class TimeWindowValidator implements Validator<Student> {
 export class SpeadsheetValidationService {
   validators = [
     new NameValidator(),
+    new AgeValidator(),
     new EmailValidator(),
     new CityValidator(),
     new CountryValidator(),
@@ -75,7 +94,7 @@ export class SpeadsheetValidationService {
   ];
 
   validateStudent(student: Student): ValidationError[] {
-    return this.validators.map(validator => validator.valdiate(student)).flat()
+    return this.validators.map(validator => validator.validate(student)).flat()
   }
 }
 
