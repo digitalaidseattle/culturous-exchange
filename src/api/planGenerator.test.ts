f49450b965833b2a035ae07a223650b4a89bedf0
jnakaso <jnakaso@yahoo.com>
2025-09-29 15:35:14 -0700

    mock test

--- src/api/planGenerator.test.ts
+++ src/api/planGenerator.test.ts
@@ -4,11 +4,76 @@
  *  @copyright 2025 Digital Aid Seattle
  *
  */
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import { placementService } from "./cePlacementService";
 import { planGenerator } from "./planGenerator";
-import { Group, Plan } from "./types";
+import { Group, Placement, Plan } from "./types";
+import { groupService } from "./ceGroupService";
+import { planService } from "./cePlanService";
+
+vi.mock("./cePlacementService", () => {
+    return {
+        placementService: {
+            updatePlacement: vi.fn(),
+        }
+    };
+});
+
+vi.mock("./ceGroupService", () => {
+    return {
+        groupService: {
+            deleteGroup: vi.fn(),
+        },
+    };
+});
+
+vi.mock("./cePlanService", () => {
+    return {
+        planService: {
+            getById: vi.fn(),
+        },
+    };
+});
 
 describe("planGenerator", () => {
+
+    it("emptyPlan", () => {
+
+        const placement = {
+            student_id: "123",
+        } as Placement;
+
+        const group = {
+            plan_id: "test",
+        } as Group;
+
+        const plan = {
+            id: "test",
+            placements: [placement],
+            groups: [group]
+        } as Plan;
+
+        const placementLessPlan = {
+            id: "test-less"
+        } as Plan;
+
+        const emptyPlan = {
+            id: "test-empty"
+        } as Plan;
+
+        (placementService.updatePlacement as ReturnType<typeof vi.fn>).mockResolvedValue(Promise.resolve(placementLessPlan));
+        (groupService.deleteGroup as ReturnType<typeof vi.fn>).mockResolvedValue(Promise.resolve());
+        (planService.getById as ReturnType<typeof vi.fn>).mockResolvedValue(Promise.resolve(emptyPlan));
+
+        planGenerator.emptyPlan(plan)
+            .then(result => {
+                expect(result).toBe(emptyPlan);
+                expect(placementService.updatePlacement).toHaveBeenCalledWith("test", "123", { group_id: null });
+                expect(groupService.deleteGroup).toHaveBeenCalledWith(group);
+                expect(planService.getById).toHaveBeenCalledWith("test");
+            })
+
+    });
 
     it("createGroups", () => {
 
