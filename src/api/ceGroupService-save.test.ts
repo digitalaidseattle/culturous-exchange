07c6f4390eb34af14d35fc88586947fc75d885c7
jnakaso <jnakaso@yahoo.com>
2025-10-09 18:15:40 -0700

    more tests

---
+++ src/api/ceGroupService-save.test.ts
@@ -0,0 +1,42 @@
+/**
+ *  planGenerator.test.ts
+ *
+ *  @copyright 2025 Digital Aid Seattle
+ *
+ */
+import { describe, expect, it, vi } from "vitest";
+import { groupService } from "./ceGroupService";
+import { timeWindowService } from "./ceTimeWindowService";
+import { Group, TimeWindow } from "./types";
+
+describe("groupService-save", () => {
+
+    it("save", () => {
+
+        const tw = {
+        } as TimeWindow;
+
+        const savedTw = {
+        } as TimeWindow;
+
+        const group = {
+            id: "test",
+            time_windows: [tw]
+        } as Group;
+
+        const inserted = {
+        } as Group;
+
+        vi.spyOn(groupService, "insert").mockResolvedValue(inserted);
+        vi.spyOn(timeWindowService, "deleteByGroupId").mockResolvedValue(true);
+        vi.spyOn(timeWindowService, "save").mockResolvedValue(savedTw);
+
+        groupService.save(group)
+            .then(result => {
+                expect(result).toBe(group);
+                expect(timeWindowService.deleteByGroupId).toHaveBeenCalledWith("test");
+                expect(timeWindowService.save).toHaveBeenCalledWith(tw);
+            })
+    });
+
+});
