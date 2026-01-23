b37948b4b8fac480ffd68a42709cdac76ea43182
jnakaso <jnakaso@yahoo.com>
2026-01-22 19:05:56 -0800

    promote import TimeWindowSelectionContext

---
+++ src/components/TimeWindowSelectionContext.tsx
@@ -0,0 +1,17 @@
+/**
+ *  TimeWindowSelectionContext.ts
+ *
+ *  @copyright 2025 Digital Aid Seattle
+ *
+ */
+import { createContext } from "react";
+
+interface TimeWindowContextType {
+    selection: string[],
+    setSelection: React.Dispatch<React.SetStateAction<string[]>>
+}
+
+export const TimeWindowSelectionContext = createContext<TimeWindowContextType>({
+    selection: [] as string[],
+    setSelection: () => []
+})
